fc120cb5a7690b848f787234e63c003052595df1
Zirfly <alex__du_42@hotmail.fr>
2020-11-23 15:19:16 +0100

    Commit initial, base commune du projet

---
+++ Obstacle.java
@@ -0,0 +1,17 @@
+package wargame;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Obstacle extends Element {
+	public enum TypeObstacle {
+		ROCHER (IConfig.COULEUR_ROCHER), FORET (IConfig.COULEUR_FORET), EAU (IConfig.COULEUR_EAU);
+		private final Color COULEUR;
+		TypeObstacle(Color couleur) { COULEUR = couleur; }
+		public static TypeObstacle getObstacleAlea() {
+			return values()[(int)(Math.random()*values().length)];
+		}
+	}
+	private TypeObstacle TYPE;
+	Obstacle(TypeObstacle type, Position pos) { TYPE = type; this.pos = pos; }
+	public String toString() { return ""+TYPE; }
+}
---
+++ PanneauJeu.java
@@ -0,0 +1,15 @@
+package wargame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zirfly 2
+ */
+public class PanneauJeu {
+    
+}
