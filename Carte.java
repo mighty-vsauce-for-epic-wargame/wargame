35dc831686cb1fb317dc3536116a46fc05f82ddf
Zirfly <alex__du_42@hotmail.fr>
2021-01-07 15:40:53 +0100

    Système Element refondu, premiers sprites mis en place

--- Carte.java
+++ Carte.java
@@ -3,94 +3,109 @@ package wargame;
 import java.awt.Color;
 import java.awt.Graphics;
 
-import wargame.Obstacle.TypeObstacle;
+public class Carte implements ICarte
+{
+    private final Element carte[][];
+    private final int VALEUR_CORRECTIVE = 4;
+    
+    public Carte()
+    {
+        carte = new Element[IConfig.LARGEUR_CARTE][IConfig.HAUTEUR_CARTE];
+        genererCarteAleatoire();
+    }
+    
+    private void genererCarteAleatoire()
+    {
+        int i, j;
+        
+        for (i = 0; i < IConfig.LARGEUR_CARTE; i++)
+            for (j = 0; j < IConfig.HAUTEUR_CARTE; j++)
+                carte[i][j] = new Element();
+    }
 
-public class Carte implements ICarte {
+    @Override
+    public Element getElement(Position pos)
+    {
+        return carte[pos.getX()][pos.getY()];
+    }
 
-	protected Element carte[][]= new Element[IConfig.LARGEUR_CARTE][IConfig.HAUTEUR_CARTE];
-	
-	@Override
-	public Element getElement(Position pos) {
-		return carte[pos.getX()][pos.getY()];
-	}
+    @Override
+    public Position trouverPositionVide()
+    {
+        return null;
+    }
 
-	@Override
-	public Position trouvePositionVide() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Position trouverPositionVide(Position pos)
+    {
+        return null;
+    }
 
-	@Override
-	public Position trouvePositionVide(Position pos) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Heros trouverHeros()
+    {
+        return null;
+    }
 
-	@Override
-	public Heros trouveHeros() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Heros trouverHeros(Position pos)
+    {
+        return null;
+    }
 
-	@Override
-	public Heros trouveHeros(Position pos) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean deplacerSoldat(Position pos, Soldat soldat)
+    {
+        return false;
+    }
 
-	@Override
-	public boolean deplaceSoldat(Position pos, Soldat soldat) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    /* WTF ? */
+    @Override
+    public void mort(Soldat perso)
+    {
+        
+    }
 
-	@Override
-	public void mort(Soldat perso) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public boolean actionHeros(Position pos, Position pos2)
+    {
+        return false;
+    }
 
-	@Override
-	public boolean actionHeros(Position pos, Position pos2) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public void jouerSoldats(PanneauJeu pj)
+    {
+        
+    }
 
-	@Override
-	public void jouerSoldats(PanneauJeu pj) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void toutDessiner(Graphics g)
+    {
+        int i, j;
+        int points[][];
+        int xOffset = (int) Hexagon.calculH(IConfig.HEX_SIZE);
+        int yOffset = (int) Hexagon.calculR(IConfig.HEX_SIZE);
 
-	@Override
-	public void toutDessiner(Graphics g) {
-		int i,j;
-		int points[][];
-		int xOffset= (int)Hexagon.calculH(IConfig.HEX_SIZE);
-		int yOffset= (int)Hexagon.calculR(IConfig.HEX_SIZE);
-	
-		for (i=0;i<IConfig.LARGEUR_CARTE;i++) {
-			for (j=0;j<IConfig.HAUTEUR_CARTE;j++) {
-				points= Hexagon.calculPoints(
-						i * IConfig.HEX_SIZE + xOffset * ( i + 1 ),
-						j * ( IConfig.HEX_SIZE - 4 /*valeur corrective */ ) + yOffset * ( Math.floorMod(i,2) + j),
-						IConfig.HEX_SIZE);
-				g.setColor(IConfig.COULEUR_VIDE);
-				g.fillPolygon(points[Hexagon.X], points[Hexagon.Y], 6);
-			}
-		}
-		
-		g.setColor(Color.BLACK);
-		
-		for (i=0;i<IConfig.LARGEUR_CARTE;i++) {
-			for (j=0;j<IConfig.HAUTEUR_CARTE;j++) {
-				points= Hexagon.calculPoints(
-						i * IConfig.HEX_SIZE + xOffset * ( i + 1 ),
-						j * ( IConfig.HEX_SIZE - 4 /*valeur corrective */ ) + yOffset * ( Math.floorMod(i,2) + j),
-						IConfig.HEX_SIZE);
-				g.drawPolygon(points[Hexagon.X], points[Hexagon.Y], 6);
-			}
-		}
-		
-	}
+        for (i = 0; i < IConfig.LARGEUR_CARTE; i++)
+        {
+            for (j = 0; j < IConfig.HAUTEUR_CARTE; j++)
+            {
+                points = Hexagon.calculPoints(
+                    i * IConfig.HEX_SIZE + xOffset * (i + 1),
+                    j * (IConfig.HEX_SIZE - VALEUR_CORRECTIVE) +
+                            yOffset * (Math.floorMod(i, 2) + j),
+                    IConfig.HEX_SIZE);
+                
+                g.drawImage(
+                        carte[i][j].getTerrainSprite(),
+                        i * IConfig.HEX_SIZE + xOffset * i,
+                        j * (IConfig.HEX_SIZE - VALEUR_CORRECTIVE) + yOffset * (Math.floorMod(i, 2) + j),
+                        null);
+                //g.fillPolygon(points[Hexagon.X], points[Hexagon.Y], 6);
+                
+                g.setColor(Color.BLACK);
+                g.drawPolygon(points[Hexagon.X], points[Hexagon.Y], 6);
+            }
+        }
+    }
 }
--- IConfig.java
+++ IConfig.java
@@ -3,17 +3,22 @@ package wargame;
 import java.awt.Color;
 
 public interface IConfig {
-	int LARGEUR_CARTE = 20;
-	int HAUTEUR_CARTE = 10;
-	int NB_PIX_CASE = 20;
-	int HEX_SIZE= 25;
-	int POSITION_X = 100;
-	int POSITION_Y = 50;
-	int NB_HEROS = 6;
-	int NB_MONSTRES = 15;
-	int NB_OBSTACLES = 20;
-	Color COULEUR_VIDE = Color.white, COULEUR_INCONNU = Color.lightGray;
-	Color COULEUR_TEXTE = Color.black, COULEUR_MONSTRES = Color.black;
-	Color COULEUR_HEROS = Color.red, COULEUR_HEROS_DEJA_JOUE = Color.pink;
-	Color COULEUR_EAU = Color.blue, COULEUR_FORET = Color.green, COULEUR_ROCHER = Color.gray;
+    int LARGEUR_CARTE = 20;
+    int HAUTEUR_CARTE = 10;
+    int NB_PIX_CASE = 20;
+    int HEX_SIZE = 25;
+    int POSITION_X = 100;
+    int POSITION_Y = 50;
+    int NB_HEROS = 6;
+    int NB_MONSTRES = 15;
+    int NB_OBSTACLES = 20;
+    Color COULEUR_VIDE = Color.white, COULEUR_INCONNU = Color.lightGray;
+    Color COULEUR_TEXTE = Color.black, COULEUR_MONSTRES = Color.black;
+    Color COULEUR_HEROS = Color.red, COULEUR_HEROS_DEJA_JOUE = Color.pink;
+
+    String SPRITE_LAC = "resources/LAC_50.png";
+    String SPRITE_FORET = "resources/FORET_50.png";
+    String SPRITE_MONTAGNE = "resources/MONTAGNE_50.png";
+    String SPRITE_PLAINE = "resources/PLAINE_50.png";
+    String SPRITE_VILLAGE = "resources/VILLAGE_50.png";
 }
