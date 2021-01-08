6bc1a547e1fd7b6c3d7fbe978bbed504c1e172cd
romanguirbal1 <guirbalroman@gmail.com>
2021-01-08 11:54:57 +0100

    added a serial number to PanneauJeu to supress the warning

--- PanneauJeu.java
+++ PanneauJeu.java
@@ -5,6 +5,10 @@ import javax.swing.JPanel;
 
 public class PanneauJeu extends JPanel{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	// creating the map
 	Carte carte= new Carte();
 	
